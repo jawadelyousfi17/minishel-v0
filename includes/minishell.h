b5689421a3e11e518160e3f62bedeacfcd09422a
Jawad <Jawad.pro17@gmail.com>
2025-02-21 23:03:10 +0100

    update : header

--- includes/minishell.h
+++ includes/minishell.h
@@ -3,6 +3,7 @@
 #include <stdbool.h>
 
 typedef enum {
+	NO,
     HER_DOC,
     APPEND,
     REDIRECT,
@@ -22,10 +23,10 @@ typedef struct s_data
     t_redirect_type redirect_output;
     char *infile;
     char *outfile;
-    t_cmd cmd;
+    t_cmd *cmd;
     bool pipe;
     int n_of_cmds;
-    t_cmd *pipe_cmd;
+    t_cmd **pipe_cmd;
 } t_data;
 
 void ft_free_t_cmd(t_cmd tcmd);
--- parser/parser.c
+++ parser/parser.c
@@ -1,2 +1,17 @@
 #include "../includes/minishell.h"
+#include <stdio.h>
 
+
+void print_str(char **s) {
+	for (size_t i = 0; s[i]; i++)
+	{
+		printf("%s\n", s[i]);
+	}
+}
+
+int main() {
+	char *s = ft_strdup("< infile ls | echo \"hello world 'How are you'\" | ls -la | grep \"Hello\" > \"outfile\"");
+	char **splited = ft_split(s, ' ');
+	printf("%s\n", s);
+	print_str(splited);
+}
