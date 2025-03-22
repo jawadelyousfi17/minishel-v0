9be0c431378bb1d5e44102f4165d29dd096c7363
Jawad <jawad.pro17@gmail.com>
2025-03-22 04:03:56 +0000

    fix: ft_export fix error message

--- builtin/export_no_args.c
+++ builtin/export_no_args.c
@@ -26,16 +26,16 @@ int ft_export_no_args(char **env)
             continue;
         }
         if (!var_name)
-            return (er4(": export: ", strerror(errno), NULL, NULL), 1);
+            return (er4("export: ", strerror(errno), NULL, NULL), 1);
         var_value = gb_get_env(env, var_name);
         if (!var_value)
         {
             if (printf("declare -x %s\n", var_name) == -1)
 
-                return (er4(": export: ", strerror(errno), NULL, NULL), 1);
+                return (er4("export: ", strerror(errno), NULL, NULL), 1);
         }
         else if (printf("declare -x %s=\"%s\"\n", var_name, var_value) == -1)
-            return (er4(": export: ", strerror(errno), NULL, NULL), 1);
+            return (er4("export: ", strerror(errno), NULL, NULL), 1);
         free(var_value);
         i++;
     }
